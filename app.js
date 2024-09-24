f7c53beadebfbddcd88a3ba7690fe843b4ea039e
Harvy <harvycardoza@gmail.com>
2024-09-24 10:32:35 -0600

    usar mongo db local
    VSC cambio las ' por "

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const path = require('path');
-const bcrypt = require('bcrypt');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const path = require("path");
+const bcrypt = require("bcrypt");
 
 //Coneccion a base de datos
-mongoose.connect('mongodb+srv://admin:123@cluster0.aqptsul.mongodb.net/?retryWrites=true&w=majority', {
+mongoose.connect("mongodb://127.0.0.1:27017", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
@@ -23,68 +23,68 @@ const userSchema = new mongoose.Schema({
   password: String,
 });
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 //Endpoint para crear un usuario
-app.post('/registro', async (req, res) => {
+app.post("/registro", async (req, res) => {
   const { username, password } = req.body;
 
   try {
-     // Encriptar la contraseña antes de guardarla
-  const hashedPassword = await bcrypt.hash(password, 10); // 10 es el costo del hash
+    // Encriptar la contraseña antes de guardarla
+    const hashedPassword = await bcrypt.hash(password, 10); // 10 es el costo del hash
 
-    const newUser = new User({ 
-      username, 
+    const newUser = new User({
+      username,
       password: hashedPassword,
     });
     await newUser.save();
-    res.status(201).json({ message: 'Usuario registrado exitosamente' });
+    res.status(201).json({ message: "Usuario registrado exitosamente" });
   } catch (error) {
-    res.status(500).json({ message: 'Error al registrar usuario' });
+    res.status(500).json({ message: "Error al registrar usuario" });
   }
 });
 
 //Endpoint para autenticar
-app.post('/login', async (req, res) => {
+app.post("/login", async (req, res) => {
   const { username, password } = req.body;
 
   try {
     const user = await User.findOne({ username });
     if (!user) {
-      return res.status(401).json({ message: 'Credenciales inválidas.' });
+      return res.status(401).json({ message: "Credenciales inválidas." });
     }
 
     const passwordMatch = await bcrypt.compare(password, user.password);
     if (!passwordMatch) {
-      return res.status(401).json({ message: 'Credenciales inválidas.' });
+      return res.status(401).json({ message: "Credenciales inválidas." });
     }
 
     if (user) {
-      res.status(200).json({ message: 'Inicio de sesión exitoso' });
+      res.status(200).json({ message: "Inicio de sesión exitoso" });
     } else {
-      res.status(401).json({ message: 'Credenciales inválidas' });
+      res.status(401).json({ message: "Credenciales inválidas" });
     }
   } catch (error) {
-    res.status(500).json({ message: 'Error al iniciar sesión' });
+    res.status(500).json({ message: "Error al iniciar sesión" });
   }
 });
 
 //Definir la carpeta publica
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // Ruta para la página de registro
-app.get('/registro', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'registro.html'));
+app.get("/registro", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "registro.html"));
 });
 
 // Ruta para la página de bienvenida
-app.get('/bienvenida', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'bienvenida.html'));
+app.get("/bienvenida", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "bienvenida.html"));
 });
 
 // Ruta para la página de documentación
-app.get('/documentacion', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'documentacion.html'));
+app.get("/documentacion", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "documentacion.html"));
 });
 
 //Iniciar app
